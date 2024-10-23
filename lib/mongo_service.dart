94822b44f5a9dfb58a778110ef0138eb10e64675
Gaby Nieva <gaby.nieva@outlook.com>
2024-10-22 23:52:25 -0600

    Adding CRUD services

--- lib/mongo_service.dart
+++ lib/mongo_service.dart
@@ -1,3 +1,4 @@
+import 'package:crud_mongodb/pet_model.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoService {
@@ -16,4 +17,28 @@ class MongoService {
   }
 
   Db get db => _db;
+
+  Future<void> insertPet(PetModel pet) async {
+    var collection = _db.collection('pets');
+    await collection.insertOne(pet.toJson());
+  }
+
+  Future<List<PetModel>> getPets() async {
+    var collection = _db.collection('pets');
+    var pets = await collection.find().toList();
+    return pets.map((pet) => PetModel.fromJson(pet)).toList();
+  }
+
+  Future<void> updatePet(PetModel pet) async {
+    var collection = _db.collection('pets');
+    await collection.updateOne(
+      where.eq('_id', pet.id),
+      modify.set('name', pet.name).set('type', pet.type),
+    );
+  }
+
+  Future<void> deletePet(String id) async {
+    var collection = _db.collection('pets');
+    await collection.remove(where.eq('_id', id)); 
+  }
 }
